116d7d90b83c70dae4feff421c8e79680c714b24
linalisna206 <linalisna206@gmail.com>
2026-04-03 10:20:25 +0700

    Menambah fungsi linked list riwayat kegiatan

--- main.cpp
+++ main.cpp
@@ -11,19 +11,37 @@ struct pet{
     int koin;
 };
 
+struct aktivitas{
+    string pesan;
+    aktivitas* next;
+};
+
+void TambahAktivitas(aktivitas* &head, string keterangan){
+aktivitas* baru = new aktivitas;
+baru-> pesan = keterangan;
+baru -> next = head;
+head = baru;
+}
+
+void LihatAktivitas(aktivitas* &head){
+    cout << "\n ====RIWAYAT AKTIVITAS PET====" << endl;
+    aktivitas* temp = head;
+    if (!temp) cout << "Belum ada aktivitas yang dilakukan" << endl;
+    while (temp) {
+        cout << "- " << temp->pesan << endl;
+        temp = temp-> next;
+    }
+    cout << "==============================" << endl;
+}
+
 int main() {
-    
-    string jenis_pet;
-    string nama_pet;
+    pet myPet;
     
     cout << "====== PET SIMULATOR ======" << endl;
     cout << "Masukan jenis pet :" << endl;
-    cin >> jenis_pet;
+    cin >> myPet.jenis;
     cout << "Masukan nama pet :" << endl;
-    cin >> nama_pet;
+    cin >> myPet.nama_pet;
 
-    cout << "======== DATA PET ========" << endl;
-    cout << "Jenis pet: " << jenis_pet << endl;
-    cout << "Nama pet: " << nama_pet << endl;
-    
- }
+   
+ };
